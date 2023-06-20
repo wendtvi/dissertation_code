59958f31dc7bd806ff51e74f39f214b00fd22f6e
wendtvi <vitoriawendt@gmail.com>
2023-06-20 10:56:12 -0300

    Update Binomial_discrete_nonlinear_wTimeInvariance.r

--- Binomial_discrete_nonlinear_wTimeInvariance.r
+++ Binomial_discrete_nonlinear_wTimeInvariance.r
@@ -44,15 +44,15 @@ mc_function=function(N){
     #Modelo de diferenças em diferença para cada cohort (01 primero é grupo e segundo é tempo: neste caso é grupo de controle no período pós trat)
     f5=rep(1,n1)*D
     f6=rep(1,n1)*D
-    
+
     matriz_estado_naotratamento=matrix(NA,nrow = n1,ncol = TT+1)
     matriz_estado_naotratamento[,TT+1]=t(D)
     for (k in 1:TT){
       for(i in 1:nrow(matriz_estado_naotratamento)){
-        if (k<q && matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pre[i]>0)
-        if (k<q && matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pre[i]>0)
-        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pos[i]>0)
-        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_trat_pos[i]>0)
+        if (k<q && matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_controle[i]+ct_0_controle_pre[i]>0)
+        if (k<q && matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_0_controle_pre[i]>0)
+        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_controle[i]+ct_0_controle_pos[i]>0)
+        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_0_trat_pos[i]>0)
         
       }
     }
@@ -62,12 +62,12 @@ mc_function=function(N){
     matriz_X_estrela[,TT+1]=t(D)
     for (k in 1:ncol(matriz_X_estrela)-1){
       for (i in 1:nrow(matriz_X_estrela)){
-        if (k<q && matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pre[i]>0)
-        if (k<q && matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pre[i]>0)
-        if (k>=q&& matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+ct_0_controle_pos[i]>0)
-        if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+rlogis(1,0,1)+ct_inf_trat_pos[i]>0)
-        if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+rlogis(1,0,1)+ct_inf_trat_pos[i]>0)
-        if (k==6 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+0.3*f6[i]+rlogis(1,0,1)+ct_inf_trat_pos[i]>0)
+        if (k<q && matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_controle[i]+ct_0_controle_pre[i]>0)
+        if (k<q && matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric((Z_cov_tratados[i,k]-1)/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_0_controle_pre[i]>0)
+        if (k>=q&& matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric((Z_cov_controle[i,k]-1)/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rlogis(1,0,1)+Z_cov_mean_controle[i]+ct_0_controle_pos[i]>0)
+        if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_inf_trat_pos[i]>0)
+        if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_inf_trat_pos[i]>0)
+        if (k==6 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+0.3*f6[i]+rlogis(1,0,1)+Z_cov_mean_tratados[i]+ct_inf_trat_pos[i]>0)
       }
     }
     
@@ -209,13 +209,13 @@ mc_function=function(N){
     ##########################################################
     #####################ESTIMADORES##########################
     ##########################################################
-    Y10=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1])*n1*pD #variável resposta observada para grupo dos tratados no período pré tratamento t=3
+    Y10=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1]) #variável resposta observada para grupo dos tratados no período pré tratamento t=3
     #Suponho que sei que variável latente segue distribuição logistica com parâmetros 0,1
-    F_Y10=pbinom((Y10), size=n1*pD, prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1]))
+    F_Y10=pbinom((Y10), size=n1*(pD), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1]))
     
-    F_inver_F_Y10_t4=qbinom(F_Y10,size=(n1*pD), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q]))
-    F_inver_F_Y10_t5=qbinom(F_Y10,size=(n1*pD), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+1]))
-    F_inver_F_Y10_t6=qbinom(F_Y10,size=(n1*pD), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+2]))
+    F_inver_F_Y10_t4=qbinom(F_Y10,size=(n1*(pD)), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q]))
+    F_inver_F_Y10_t5=qbinom(F_Y10,size=(n1*(pD)), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+1]))
+    F_inver_F_Y10_t6=qbinom(F_Y10,size=(n1*(pD)), prob=mean(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+2]))
     
     
     ##########################################################
