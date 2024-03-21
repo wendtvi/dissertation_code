ba6562fe949d73e77a1cd0849ce42cc878748f02
wendtvi <vitoriawendt@gmail.com>
2024-03-21 17:19:10 -0300

    Update police_complaints_application.r

--- police_complaints_application.r
+++ police_complaints_application.r
@@ -1,6 +1,6 @@
 #install.packages("staggered")
 library(staggered)
-library(energy)
+#library(energy)
 
 setwd("C:/Users/vitor/Downloads")
 load(file='pj_officer_level_balanced.rda')
@@ -8,6 +8,7 @@ dados=as.data.frame(pj_officer_level_balanced)
 dados=dados[dados$first_trained>13,]
 dados=dados[dados$first_trained<72,]
 #dados=dados[sort(dados$uid,decreasing = FALSE),]
+#dados=dados[sort(dados$first_trained,decreasing = FALSE),]
 #dados[,ncol(dados)+1]=NA
 #names(dados)[ncol(dados)]="force_temp"
 #for (i in 1:unique(dados$uid)){
@@ -16,102 +17,107 @@ dados=dados[dados$first_trained<72,]
 
 #dados=dados[dados$force_temp<1,]
 
-
-minha_base=data.frame(unique(dados$uid),0,0,0,0)
-names(minha_base)=c("ID","COMPLAINTS_BEFORE","COMPLAINTS_AFTER","INTERVENTION_PERIOD","TREATED_GROUP")
-
-##################################################
-############EXPERIMENTO 2X2#######################
-##################################################
-t_interesse=53
-
-c=1
-k=0
-i=0
-dados_temp=dados[dados$period>=t_interesse-1,]
-dados_temp=dados[dados$period<=t_interesse,]
-for (i in unique(dados_temp$uid)){
-  k=k+1
-  while (dados_temp$uid[c]==i) {
-    minha_base$INTERVENTION_PERIOD[k]=dados_temp$first_trained[c]
-    if(dados_temp$period[c]==t_interesse-1) minha_base$COMPLAINTS_BEFORE[k]=minha_base$COMPLAINTS_BEFORE[k]+dados_temp$complaints[c]
-    if(dados_temp$period[c]==t_interesse) minha_base$COMPLAINTS_AFTER[k]=minha_base$COMPLAINTS_AFTER[k]+dados_temp$complaints[c]
-    c=c+1
-  }
+matriz_estimadores_dinamicas=matrix(NA,length(unique(dados$first_trained))-1,4)
+l_matriz=0
+max_period=max(dados$period)
+for (p in sort(unique(dados$first_trained))[-1]){
+  l_matriz=l_matriz+1
+    t_interesse_1=sort(unique(dados$first_trained))[which(sort(unique(dados$first_trained))==p)-1]
+  
+    minha_base=data.frame(unique(dados$uid),0,0,0,0)
+    names(minha_base)=c("ID","COMPLAINTS_BEFORE","COMPLAINTS_AFTER","INTERVENTION_PERIOD","TREATED_GROUP")
+    
+    ##################################################
+    ############EXPERIMENTO 2X2#######################
+    ##################################################
+    t_interesse=p
+    
+    c=1
+    k=0
+    i=0
+    dados_temp=dados[dados$first_trained>=t_interesse_1,]
+    dados_temp=dados[dados$first_trained<=t_interesse,]
+    for (i in unique(dados_temp$uid)){
+      k=k+1
+      while (dados_temp$uid[c]==i && c<=length(dados_temp$uid)) {
+        minha_base$INTERVENTION_PERIOD[k]=dados_temp$first_trained[c]
+        if(dados_temp$period[c]==t_interesse_1) minha_base$COMPLAINTS_BEFORE[k]=minha_base$COMPLAINTS_BEFORE[k]+dados_temp$complaints[c]
+        if(dados_temp$period[c]==t_interesse) minha_base$COMPLAINTS_AFTER[k]=minha_base$COMPLAINTS_AFTER[k]+dados_temp$complaints[c]
+        c=c+1
+      }
+    }
+    
+    #escolho o período 28 como unico periodo de tratamento (quem recebeu tratamento em t = 28 é grupo tratado, quem recebeu após t=28 é grupo controle)
+    minha_base$TREATED_GROUP=as.numeric(minha_base$INTERVENTION_PERIOD==t_interesse)
+    #minha_base=minha_base[minha_base$INTERVENTION_PERIOD>=t_interesse,]
+    
+    #####################################
+    #############CIC ESTIMATOR###########
+    #####################################
+    Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
+    Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
+    Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
+    Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
+    
+    #LB
+    F00_hat_LB=vector()
+    for (i in 1:length(Y_10)) { F00_hat_LB[i]=mean(Y_00<=Y_10[i]) }
+    F_inver_01_hat_LB=vector()
+    F01_hat_LB=vector()
+    for (i in 1:length(Y_01)) { F01_hat_LB[i]=mean(Y_01<=Y_01[i]) }
+    for (i in 1:length(F00_hat_LB)) { F_inver_01_hat_LB[i]=min(Y_01[F01_hat_LB>=F00_hat_LB[i]]) }
+    ATT_CIC_LB=mean(Y_11)-mean(F_inver_01_hat_LB)
+    
+    #UB
+    F00_hat_UB=vector()
+    for (i in 1:length(Y_10)) { F00_hat_UB[i]=mean(Y_00<Y_10[i]) }
+    F_inver_01_hat_UB=vector()
+    F01_hat_UB=vector()
+    for (i in 1:length(Y_01)) { F01_hat_UB[i]=mean(Y_01<Y_01[i]) }
+    for (i in 1:length(F00_hat_UB)) { F_inver_01_hat_UB[i]=min(Y_01[F01_hat_UB>F00_hat_UB[i]]) }
+    ATT_CIC_UB=mean(Y_11)-mean(F_inver_01_hat_UB)
+    
+    
+    
+    #####################################
+    #############MEU ESTIMATOR###########
+    #####################################
+    Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
+    Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
+    Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
+    Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
+    
+    F00_hat_meu=ppois(Y_10, lambda = mean(Y_00))
+    F_inver_01_hat_meu=qpois(F00_hat_meu, lambda = mean(Y_01))
+    
+    ATT_meu=mean(Y_11)-mean(F_inver_01_hat_meu)
+    
+    
+    
+    #####################################
+    ########WOOLDRIDGE ESTIMATOR#########
+    #####################################
+    Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
+    Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
+    Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
+    Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
+    
+    ATT_WOOLD=mean(Y_11)-exp(log(mean(Y_10))+(log(mean(Y_01))-log(mean(Y_00))))
+    
+    
+    #################################################################
+    #resumo resultados
+    #CIC
+    matriz_estimadores_dinamicas[l_matriz,1]=ATT_CIC_UB
+    matriz_estimadores_dinamicas[l_matriz,2]=ATT_CIC_LB
+    
+    #MEU
+    matriz_estimadores_dinamicas[l_matriz,3]=ATT_meu
+    
+    #WOOLDRIDGE
+    matriz_estimadores_dinamicas[l_matriz,4]=ATT_WOOLD
+    
 }
-
-#escolho o período 28 como unico periodo de tratamento (quem recebeu tratamento em t = 28 é grupo tratado, quem recebeu após t=28 é grupo controle)
-minha_base$TREATED_GROUP=as.numeric(minha_base$INTERVENTION_PERIOD==t_interesse)
-minha_base=minha_base[minha_base$INTERVENTION_PERIOD>=t_interesse,]
-
-#####################################
-#############CIC ESTIMATOR###########
-#####################################
-Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
-Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
-Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
-Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
-
-#LB
-F00_hat_LB=vector()
-for (i in 1:length(Y_10)) { F00_hat_LB[i]=mean(Y_00<=Y_10[i]) }
-F_inver_01_hat_LB=vector()
-F01_hat_LB=vector()
-for (i in 1:length(Y_01)) { F01_hat_LB[i]=mean(Y_01<=Y_01[i]) }
-for (i in 1:length(F00_hat_LB)) { F_inver_01_hat_LB[i]=min(Y_01[F01_hat_LB>=F00_hat_LB[i]]) }
-ATT_CIC_LB=mean(Y_11)-mean(F_inver_01_hat_LB)
-
-#UB
-F00_hat_UB=vector()
-for (i in 1:length(Y_10)) { F00_hat_UB[i]=mean(Y_00<Y_10[i]) }
-F_inver_01_hat_UB=vector()
-F01_hat_UB=vector()
-for (i in 1:length(Y_01)) { F01_hat_UB[i]=mean(Y_01<Y_01[i]) }
-for (i in 1:length(F00_hat_UB)) { F_inver_01_hat_UB[i]=min(Y_01[F01_hat_UB>F00_hat_UB[i]]) }
-ATT_CIC_UB=mean(Y_11)-mean(F_inver_01_hat_UB)
-
-
-
-#####################################
-#############MEU ESTIMATOR###########
-#####################################
-Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
-Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
-Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
-Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
-
-F00_hat_meu=ppois(Y_10, lambda = mean(Y_00))
-F_inver_01_hat_meu=qpois(F00_hat_meu, lambda = mean(Y_01))
-
-ATT_meu=mean(Y_11)-mean(F_inver_01_hat_meu)
-
-
-
-#####################################
-########WOOLDRIDGE ESTIMATOR#########
-#####################################
-Y_10=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==1]
-Y_11=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==1]
-Y_00=minha_base$COMPLAINTS_BEFORE[minha_base$TREATED_GROUP==0]
-Y_01=minha_base$COMPLAINTS_AFTER[minha_base$TREATED_GROUP==0]
-
-ATT_WOOLD=mean(Y_11)-exp(log(mean(Y_10))+(log(mean(Y_01))-log(mean(Y_00))))
-
-
-#################################################################
-#resumo resultados
-#CIC
-ATT_CIC_UB
-ATT_CIC_LB
-(abs(ATT_CIC_UB)+abs(ATT_CIC_LB))/2
-
-#MEU
-ATT_meu
-
-#WOOLDRIDGE
-ATT_WOOLD
-
-
 
 ##################################################
 ############EXPERIMENTO STAGGERED#################
