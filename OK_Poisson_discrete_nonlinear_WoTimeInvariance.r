c1fe2559627b295faca4043fb0941a35a93851dd
wendtvi <vitoriawendt@gmail.com>
2024-04-12 06:30:29 -0300

    Update OK_Poisson_discrete_nonlinear_WoTimeInvariance.r

--- OK_Poisson_discrete_nonlinear_WoTimeInvariance.r
+++ OK_Poisson_discrete_nonlinear_WoTimeInvariance.r
@@ -215,9 +215,9 @@ mc_function=function(N){
     #Suponho que sei que variável latente segue distribuição logistica com parâmetros 0,1
     F_Y10=ppois((Y10), lambda = sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
     
-    F_inver_F_Y10_t4=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
-    F_inver_F_Y10_t5=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+1])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
-    F_inver_F_Y10_t6=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+2])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q-1])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
+    F_inver_F_Y10_t4=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
+    F_inver_F_Y10_t5=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+1])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+1])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
+    F_inver_F_Y10_t6=qpois(F_Y10, lambda =sum(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q+2])*(length(matriz_X_estrela[matriz_X_estrela[,TT+1]==1,q+2])/length(matriz_X_estrela[matriz_X_estrela[,TT+1]==0,q-1])))
     
     
     ##########################################################
