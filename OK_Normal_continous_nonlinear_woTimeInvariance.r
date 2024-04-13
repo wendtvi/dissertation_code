3271b807eb1a76a565a28641b7394ef09f67b1e2
wendtvi <vitoriawendt@gmail.com>
2024-04-13 15:17:20 -0300

    Update OK_Normal_continous_nonlinear_woTimeInvariance.r

--- OK_Normal_continous_nonlinear_woTimeInvariance.r
+++ OK_Normal_continous_nonlinear_woTimeInvariance.r
@@ -16,20 +16,27 @@ mc_function=function(N){
     #####################CENÁRIO SIMULAÇÃO####################
     ##########################################################
     
-    
     #Gerando covariável
-    Z_cov_mean=vector()
-    Z_cov=matrix(NA,ncol=TT,nrow=n1)
-    for (k in 1:nrow(Z_cov)){
-      Z_cov[k,]=rexp(TT,1)
-      Z_cov_mean[k]=mean(Z_cov[k,])
+    Z_cov_mean_tratados=vector()
+    Z_cov_tratados=matrix(NA,ncol=TT,nrow=n1)
+    for (k in 1:nrow(Z_cov_tratados)){
+      Z_cov_tratados[k,]=rexp(TT,1)
+      Z_cov_mean_tratados[k]=mean(Z_cov_tratados[k,])
+    }
+    
+    Z_cov_mean_controle=vector()
+    Z_cov_controle=matrix(NA,ncol=TT,nrow=n1)
+    for (k in 1:nrow(Z_cov_controle)){
+      Z_cov_controle[k,]=rexp(TT,1)
+      Z_cov_mean_controle[k]=mean(Z_cov_controle[k,])
     }
     
     #Variável de tratamento
     V=rlogis(n1,0,1)
     D=vector()
-    D=rep(-0.5,n1)+(Z_cov_mean-1)+V>0
+    D=rep(-0.5,n1)+(Z_cov_mean_tratados-1)+V>0
     pD=sum(D)/length(D) #Incidência de tratamento na população
+    D=as.numeric(D)
     
     ct_0_controle_pre=rep(0,n1)
     ct_0_controle_pos=rep(0,n1)
@@ -47,10 +54,10 @@ mc_function=function(N){
     matriz_estado_naotratamento[,TT+1]=t(D)
     for (k in 1:TT){
       for(i in 1:nrow(matriz_estado_naotratamento)){
-        if (k<q && matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_controle_pre[i])
-        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_controle_pos[i])
-        if (k<q && matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_trat_pre[i])
-        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_trat_pos[i])
+        if (k<q && matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_controle[i,k]*1/k,1)+ct_0_controle_pre[i])
+        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==0) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_controle[i,k]*1/k,1)+ct_0_controle_pos[i])
+        if (k<q && matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_tratados[i,k]-1)*5/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_0_trat_pre[i])
+        if (k>=q&& matriz_estado_naotratamento[i,TT+1]==1) matriz_estado_naotratamento[i,k]=as.numeric(-1+(Z_cov_tratados[i,k]-1)*5/2-2*D[i]+(Z_cov_tratados[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_0_trat_pos[i])
         
       }
     }
@@ -60,13 +67,13 @@ mc_function=function(N){
     matriz_X_estrela[,TT+1]=t(D)
     for (k in 1:ncol(matriz_X_estrela)-1){
       for (i in 1:nrow(matriz_X_estrela)){
-        if (k<q && matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_controle_pre[i])
-        if (k>=q&& matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_controle_pos[i])
-        if (k<q && matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_trat_pre[i])
-        if (k>=q&& matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_mean[i]-1)*5/2-2*D[i]+(Z_cov_mean[i]-1)*D[i]/4+rnorm(1,k/(2*TT),1)+ct_0_trat_pos[i])
-        if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_mean[i]-1)-2*D[i]+rnorm(1,k/(2*TT),1)+ct_inf_trat_pos[i])
-        if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_mean[i]-1)-2*D[i]+0.2*f5[i]+rnorm(1,k/(2*TT),1)+ct_inf_trat_pos[i])
-        if (k==6 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_mean[i]-1)-2*D[i]+0.2*f5[i]+0.3*f6[i]+rnorm(1,k/(2*TT),1)+ct_inf_trat_pos[i])
+        if (k<q && matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_controle[i,k]*1/k,1)+ct_0_controle_pre[i])
+        if (k>=q&& matriz_X_estrela[i,TT+1]==0) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_controle[i,k]*1/k,1)+ct_0_controle_pos[i])
+        if (k<q && matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_0_trat_pre[i])
+        if (k>=q&& matriz_X_estrela[i,TT+1]==1) matriz_X_estrela[i,k]=as.numeric(-1+(Z_cov_controle[i,k]-1)*5/2-2*D[i]+(Z_cov_controle[i,k]-1)*D[i]/4+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_0_trat_pos[i])
+        if (k==4 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_inf_trat_pos[i])
+        if (k==5 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_inf_trat_pos[i])
+        if (k==6 && matriz_X_estrela[i,TT+1]==1)matriz_X_estrela[i,k]=as.numeric(-1+0.5+(Z_cov_tratados[i,k]-1)-2*D[i]+0.2*f5[i]+0.3*f6[i]+rnorm(1,rnorm(1,0,1)+Z_cov_tratados[i,k]*1/k,1)+ct_inf_trat_pos[i])
       }
     }
     
